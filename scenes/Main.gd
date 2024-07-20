e6b34b8e84b96ea4dd51ba4053fbebbbafd279da
Jason Espin <jespin@golfbreaks.com>
2024-07-20 23:33:43 +0100

    Removes unecessary line

--- scenes/Main.gd
+++ scenes/Main.gd
@@ -3,6 +3,5 @@ extends Node
 var dynamic_object_speed : int = 700
 
 func _process(delta):
-	#position.x -= delta * dynamic_object_speed
 	for dynamic_object in get_tree().get_nodes_in_group("DynamicObject"):
 		dynamic_object.position.x -= delta * dynamic_object_speed
